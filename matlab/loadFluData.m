35d12b1b33881e2a1e7d59c63d365cd4de32ceb7
Logan Dorman <ldorman2012@gmail.com>
2020-08-02 17:50:58 -0400

    Pull totals and reformat corresponding dates from CSV file.

--- matlab/loadFluData.m
+++ matlab/loadFluData.m
@@ -1,16 +1,32 @@
-function [fluDat] = loadFluData(csvFile)
+function [fluDates, fluTotals] = loadFluData(csvFile)
 
-    tmp = readmatrix(csvFile, 'NumHeaderLines', 1, 'OutputType', 'char');
+    rawCSV = readmatrix(csvFile, 'NumHeaderLines', 1, 'OutputType', 'string');
     
     % locate rows with "Influenza_Total" and "Influenza Tested" for
     % category
+    category_index = find(contains(rawCSV(:,5),["Influenza_Total"])); %, "Influenza Tested"]));
+
     % only pull "California" region, start with whole state instead of just
     % counties
+    location_index = find(contains(rawCSV(:,4),"California"));
     
-    % convert the date code into a datetime (current format is YYYYWW)
+    desiredRows = intersect(category_index, location_index);
+    
+    % pull date, area, type of data, and number for each desired row
+    rawDataSubset = rawCSV(desiredRows, [3 4 5 6]);
+    
+    % convert the date code into a datetime (MM/DD/YY)
     % season runs from week 40 to week 39 the following year
+    rawDate = double(split(rawDataSubset(:,1), '/'));
+    fluDates = datetime(2000+rawDate(:,3), rawDate(:,1), rawDate(:,2));
     
-    % for a given week return the total, number tested, and positive test
-    % ratio
+    
+   fluTotals = double(rawDataSubset(:,4));
 
 end
+
+% table this for now, may return to it later
+
+% for a given week return the total, number tested, and positive test
+    % ratio
+    
